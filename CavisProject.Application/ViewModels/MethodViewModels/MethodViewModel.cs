7b0c6854be42e96df80c0cb612f0a695e96ff6c5
tinhle2607 <131324115+tinhle2607@users.noreply.github.com>
2024-06-28 00:42:01 +0700

    Add ViewModel

--- CavisProject.Application/ViewModels/MethodViewModels/MethodViewModel.cs
+++ CavisProject.Application/ViewModels/MethodViewModels/MethodViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CavisProject.Domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,13 @@ namespace CavisProject.Application.ViewModels.MethodViewModels
 {
     public class MethodViewModel
     {
-        public string Id { get; set; }
-        public string MethodName { get; set; }
-        public string Description { get; set; }
-        public Guid? CreatedBy { get; set; }
+        public string? Id { get; set; }
+        public string? MethodName { get; set; }
+        public string? Description { get; set; }
+        public string? URLImage { get; set; }
+        public string? UserName { get; set; }
+        public MethodStatusEnum? Status { get; set; }
+        public string? UserAvatar { get; set; }
+        
     }
 }
