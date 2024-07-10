504cd538612d472fb2507588f4a9b705de2f9ac9
tinhle2607 <131324115+tinhle2607@users.noreply.github.com>
2024-07-10 20:24:12 +0700

    fix Appointment Controller

--- CavisProject.API/Controllers/AppointmentController.cs
+++ CavisProject.API/Controllers/AppointmentController.cs
@@ -18,11 +18,6 @@ namespace CavisProject.API.Controllers
         [SwaggerOperation(Summary = "lấy danh sách các cuộc hẹn")]
         [HttpGet("mine")]
         public async Task<ApiResponse<Pagination<AppointmentViewModel>>> GetWeeklyScheduleAsync( AvailableExpertSkincareFilterViewModel filter) => await _appointmentService.GetWeeklyScheduleAsync(filter);
-        [SwaggerOperation(Summary = "đặt lịch tư vấn với chuyên gia skin care")]
-        [HttpPost("mine/skin-care")]
-        public async Task<ApiResponse<bool>> BookAppointmentAsync([FromBody]CreateAppointmentViewModel create)=> await _appointmentService.BookAppointmentAsync(create);
-        [SwaggerOperation(Summary = "đặt lịch tư vấn với chuyên gia make up")]
-        [HttpPost("mine/make-up")]
-        public async Task<ApiResponse<bool>> BookMakeUpAppointment([FromBody]CreateMakeUpAppointmentViewModel create) => await _appointmentService.BookMakeUpAppointment(create);
+        
     }
 }
